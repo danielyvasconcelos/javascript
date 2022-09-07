543807a22f26e5d567c83a07c4a6e680fe8432b0
danielyvasconcelos <desv1@aluno.ifal.edu.br>
2022-09-07 18:47:18 -0300

    finalizando aula 03

---
+++ Js_e_Node/console-api.js
@@ -0,0 +1,11 @@
+/**
+ * ? log é um registro 
+ * ! error é um tratamento de erro ja pre definido
+ * *console.table() para visualizar de forma mais organizada informações tabulares;
+ * ? console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+ * ! console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução
+ */
+
+
+//tratamento de erro
+console.error('deu error!')
