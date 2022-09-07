f38c6c524ad22853be7518270c91a018617b3dc7
danielyvasconcelos <desv1@aluno.ifal.edu.br>
2022-09-07 17:14:25 -0300

    exemplos da aula 04

---
+++ conversoes/conversoesDeTipos.js
@@ -0,0 +1,12 @@
+// node conversoesDeTipos.js
+// tipos de dados 
+// booleanos 
+
+// conversao implícita
+const numero = 456
+const numeroString = "456"
+console.log(numero== numeroString) // dois iguais compara 
+console.log(numero + numeroString) // transforma o numero em ums string , ou seja concatenoou , ou melhor, juntou
+// conversão explícita
+//Number() e String()
+console.log(numero + Number(numeroString)) // podemos converter direto da constante 
